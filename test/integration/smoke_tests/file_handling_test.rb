359f9268b0a7ad9432f81f4ac2c56809237f03b4
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-21 00:25:43 +0100

    Added some more skipped tests

---
+++ test/integration/smoke_tests/file_handling_test.rb
@@ -0,0 +1,33 @@
+require "./test/test_helper"
+
+module Lucio
+  describe "File Handling" do
+    context "Valid files" do
+      it "128kbps CBR MP3" do
+        skip
+      end
+
+      it "320kbps CBR MP3" do
+        skip
+      end
+
+      it "Quality 0 VBR MP3" do
+        skip
+      end
+
+      it "Quality 4 VBR MP3" do
+        skip
+      end
+
+      it "Quality 9 VBR MP3" do
+        skip
+      end
+    end
+
+    context "Invalid files" do
+      it "Throws an error if an invalid MP3 file was given" do
+        skip
+      end
+    end
+  end
+end
---
+++ test/unit/lib/boombox/mp3_player_test.rb
@@ -0,0 +1,17 @@
+require "./test/test_helper"
+
+module Lucio
+  describe MP3Player do
+    describe "MP3Player.new(mp3_file)" do
+      it "Returns without error" do
+        skip
+      end
+    end
+
+    describe "#current_sample" do
+      it "Returns the current sample based on the amount of time that has passed" do
+        skip
+      end
+    end
+  end
+end
