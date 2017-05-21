9b46239d51fc033a4b4316b8bb65bf60a09277c5
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-21 18:21:09 +0100

    Added basic File test stubs tomato other failing tests pass

--- lib/boombox/mp3/file.rb
+++ lib/boombox/mp3/file.rb
@@ -1,6 +1,9 @@
 module Boombox
   module MP3
     class File
+      def initialize(frames)
+        @frames = frames
+      end
     end
   end
 end
--- test/unit/lib/boombox/mp3/file_test.rb
+++ test/unit/lib/boombox/mp3/file_test.rb
@@ -5,17 +5,17 @@ module Boombox
     describe File do
       describe "Boombox::MP3::File.new(frame_data)" do
         it "Creates a new MP3 file data object" do
-          skip
+          frame_data = []
+          MP3::File.new(frame_data)
         end
       end
 
-      describe "Public Methods" do
+      describe "Instance Methods" do
         describe "#bit_rate" do
         end
 
         describe "#sample_rate" do
         end
-        
         describe "#frames" do
         end
       end
