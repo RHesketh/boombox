485141c57cf5bcf768e0454751290c6358301461
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-21 18:21:38 +0100

    Added basic file parser implementation

--- lib/boombox/mp3/parser.rb
+++ lib/boombox/mp3/parser.rb
@@ -1,8 +1,41 @@
+require_relative 'parser/seekable_buffer'
+
 module Boombox
   module MP3
     class Parser
       def self.load_from_file(mp3_file)
+        mp3_data = ::File.open(mp3_file)
+        buffer = SeekableBuffer.new(mp3_data)
+        frame_data = parse_frames_from_data(buffer)
 
+        MP3::File.new(frame_data)
+      end
+
+      private
+
+      def self.parse_frames_from_data(buffer)
+        frames = []
+
+        until buffer.eof?
+          frame = Frame.new
+
+          until frame.header_complete? || buffer.eof?
+            frame << buffer.read
+
+            if frame.invalid?
+              buffer.reset_position
+              frame.clear_bytes
+            end
+          end
+
+          unless buffer.eof?
+            frame << buffer.read(frame.bytes.length)
+            buffer.advance_marker
+            frames << frame
+          end
+        end
+
+        frames
       end
     end
   end
--- test/unit/lib/boombox/mp3/parser_test.rb
+++ test/unit/lib/boombox/mp3/parser_test.rb
@@ -5,8 +5,13 @@ module Boombox
     describe Parser do
       describe "Parser.load_from_file(mp3_file)" do
         it "Returns a parsed MP3 file" do
-          skip
-          file = Parser.load_from_file("file-location-on-disk")
+          @file_location = "file-location-on-disk"
+          mock_data = mock("IO")
+          mock_buffer = mock("Boombox::MP3::Parser::SeekableBuffer")
+          ::File.expects(:open).with(@file_location).returns(mock_data)
+          Parser::SeekableBuffer.expects(:new).with(mock_data).returns(mock_buffer)
+          mock_buffer.expects(:eof?).returns true
+          file = Parser.load_from_file(@file_location)
 
           assert_equal MP3::File, file.class
         end
