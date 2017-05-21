acffd42c7b5dbf579e11c2d19292cc003f14cf49
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-21 18:20:27 +0100

    Tidying up require tree a bit

--- lib/boombox.rb
+++ lib/boombox.rb
@@ -1,12 +1,6 @@
-# Shoutcast server
+
 require_relative 'boombox/server/shoutcast'
 
-# MP3 file handling
-require_relative 'boombox/mp3/file'
-require_relative 'boombox/mp3/parser'
-require_relative 'boombox/mp3/parser/buffer'
-require_relative 'boombox/mp3/player'
-require_relative 'boombox/mp3/playlist'
+require_relative 'boombox/mp3'
 
-# Misc
 require_relative 'boombox/version'
---
+++ lib/boombox/mp3.rb
@@ -0,0 +1,9 @@
+require_relative 'mp3/frame'
+require_relative 'mp3/file'
+require_relative 'mp3/parser'
+require_relative 'mp3/player'
+require_relative 'mp3/playlist'
+
+module MP3
+  SAMPLES_PER_FRAME = 1152
+end
