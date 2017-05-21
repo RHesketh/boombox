e489bf11dec67793cfaaa91c8950376cdb36e55e
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-22 00:05:56 +0100

    Validation refactoring/clarification. The purpose of the Validation is to look for frame that are *definitely invalid*, rather than ones which *might be invalid*.

--- lib/boombox/mp3/frame/validation.rb
+++ lib/boombox/mp3/frame/validation.rb
@@ -5,10 +5,11 @@ module Boombox
         def self.invalid?(frame_bytes)
           bits = frame_bytes[0..3].map{|b| '%0*b' % [8,b]}.join
 
-          return true unless frame_sync_valid? bits
-          return true unless is_mpeg_version_1? bits
-          return true unless is_mpeg_layer_3? bits
-          return true unless bit_rate_set? bits
+          return true if frame_sync_head_invalid? bits
+          return true if frame_sync_tail_invalid? bits
+          return true if is_wrong_mpeg_version? bits
+          return true if is_wrong_mpeg_layer? bits
+          return true if bit_rate_is_free? bits
           return true if bit_rate_error? bits
           return true if reserved_sample_rate? bits
 
@@ -17,34 +18,53 @@ module Boombox
 
         private
 
-        def self.frame_sync_valid?(bits)
-          return true if bits[0..10] == "11111111111"
-          return false
-        end
-
-        def self.is_mpeg_version_1?(bits)
-          return true if bits[11..12] == "11" # MPEG version 1
-          return false
-        end
-
-        def self.is_mpeg_layer_3?(bits)
-          return true if bits[13..14] == "01" # Layer III
-          return false
-        end
-
-        def self.bit_rate_set?(bits)
-          return false if bits[16..19] == "0000"  # "Free" bit rate
+        def self.frame_sync_head_invalid?(bits)
+          return false unless bits_present?(bits, (0..7))
+          return false if bits[0..7] == "11111111"
           return true
         end
 
+        def self.frame_sync_tail_invalid?(bits)
+          return false unless bits_present?(bits, (8..10))
+          return false if bits[8..10] == "111"
+          return true
+        end
+
+        def self.is_wrong_mpeg_version?(bits)
+          return false unless bits_present?(bits, (11..12))
+          return true if bits[11..12] == "00" # MPEG version "2.5"
+          return true if bits[11..12] == "01" # Reserved
+          return false
+        end
+
+        def self.is_wrong_mpeg_layer?(bits)
+          return false unless bits_present?(bits, (13..14))
+          return false if bits[13..14] == "01" # Layer III
+          return true
+        end
+
+        def self.bit_rate_is_free?(bits)
+          return false unless bits_present?(bits, (16..19))
+          return true if bits[16..19] == "0000"  # "Free" bit rate i.e. undefined
+          return false
+        end
+
         def self.bit_rate_error?(bits)
+          return false unless bits_present?(bits, (16..19))
           return true if bits[16..19] == "1111"   # Bit rate error
           return false
         end
 
         def self.reserved_sample_rate?(bits)
+          return false unless bits_present?(bits, (20..21))
           return true if bits[20..21] == "11"  # 11 = reserved
           return false
+        end
+
+        def self.bits_present?(bits, range)
+          return false if bits[range].nil?
+          return false if bits[range].length < range.size
+          return true
         end
       end
     end
