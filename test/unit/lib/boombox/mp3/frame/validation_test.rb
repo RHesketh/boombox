9f39cfb90fcecd58cc2272a240d3877effdb70e4
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-22 00:06:57 +0100

    Make sure our header validation properly handles both MPEGv1 and MPEGv2 MP3 files

--- test/unit/lib/boombox/mp3/frame/validation_test.rb
+++ test/unit/lib/boombox/mp3/frame/validation_test.rb
@@ -6,17 +6,34 @@ module Boombox
     describe Frame::Validation do
       describe "Class Methods" do
         describe "#invalid?(bytes)" do
-          it "Returns false when a valid frame header is passed in" do
-            assert_equal false, Frame::Validation.invalid?(Test::FrameData.valid_header)
+          context "MPEGv1 Headers" do
+            it "Returns false when a valid frame header is passed in" do
+              assert_equal false, Frame::Validation.invalid?(Test::FrameData::MPEGv1.valid_header)
+            end
+
+            it "Returns true when an invalid frame header is passed in" do
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv1.frame_sync_missing)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv1.wrong_version)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv1.wrong_layer)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv1.no_bitrate_set)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv1.bit_rate_error)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv1.reserved_sample_rate)
+            end
           end
 
-          it "Returns true when an invalid frame header is passed in" do
-            assert_equal true, Frame::Validation.invalid?(Test::FrameData.frame_sync_missing)
-            assert_equal true, Frame::Validation.invalid?(Test::FrameData.wrong_version)
-            assert_equal true, Frame::Validation.invalid?(Test::FrameData.wrong_layer)
-            assert_equal true, Frame::Validation.invalid?(Test::FrameData.no_bitrate_set)
-            assert_equal true, Frame::Validation.invalid?(Test::FrameData.bit_rate_error)
-            assert_equal true, Frame::Validation.invalid?(Test::FrameData.reserved_sample_rate)
+          context "MPEGv2 Headers" do
+            it "Returns false when a valid frame header is passed in" do
+              assert_equal false, Frame::Validation.invalid?(Test::FrameData::MPEGv2.valid_header)
+            end
+
+            it "Returns true when an invalid frame header is passed in" do
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv2.frame_sync_missing)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv2.wrong_version)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv2.wrong_layer)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv2.no_bitrate_set)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv2.bit_rate_error)
+              assert_equal true, Frame::Validation.invalid?(Test::FrameData::MPEGv2.reserved_sample_rate)
+            end
           end
         end
       end
