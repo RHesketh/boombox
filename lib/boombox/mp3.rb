599d71f4310b7a84abc77089d87839698c090df4
Rob Hesketh <rhesketh.uk@gmail.com>
2017-05-21 23:59:09 +0100

    Added BITS_PER_SAMPLE constant for doing MP3 maths

--- lib/boombox/mp3.rb
+++ lib/boombox/mp3.rb
@@ -4,6 +4,9 @@ require_relative 'mp3/parser'
 require_relative 'mp3/player'
 require_relative 'mp3/playlist'
 
-module MP3
-  SAMPLES_PER_FRAME = 1152
+module Boombox
+  module MP3
+    SAMPLES_PER_FRAME = 1152
+    BITS_PER_SAMPLE = 144
+  end
 end
--- test/unit/lib/boombox/mp3_test.rb
+++ test/unit/lib/boombox/mp3_test.rb
@@ -1,9 +1,17 @@
 require "./test/test_helper"
 
-describe MP3 do
-  describe "#SAMPLES_PER_FRAME" do
-    it "Returns 1152, the hardcoded number of samples in an MP3 frame" do
-      assert_equal 1152, MP3::SAMPLES_PER_FRAME
+module Boombox
+  describe MP3 do
+    describe "#SAMPLES_PER_FRAME" do
+      it "Returns 1152, the hardcoded number of samples in an MP3 frame" do
+        assert_equal 1152, MP3::SAMPLES_PER_FRAME
+      end
+    end
+
+    describe "#BITS_PER_SAMPLE" do
+      it "Returns 144, the hardcoded number of bits in an MP3 sample" do
+        assert_equal 144, MP3::BITS_PER_SAMPLE
+      end
     end
   end
 end
